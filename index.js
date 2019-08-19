399c1201108fdd3a5ef24b0c6215d509d8294d49
DVo-MidwayUSA <dvo@midwayusa.com>
2019-08-19 17:11:59 -0500

    first dom displays

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import path from "path"
+
+import data from "./data-access"
+
+const app = new express()
+const PORT = 8888
+
+const byId = (a, b) => {
+  let comparison = 0
+  if (a.id > b.id) comparison = 1
+  else comparison = -1
+  return comparison
+}
+
+app.get("/api/nodes", (request, response) => {
+  data.get("nodes").then(nodes => {
+    const sorted = [...nodes].sort(byId)
+    response.send(sorted).status(200)
+  })
+})
+
+app.use("/", express.static(path.join(__dirname, "./public")))
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log("CTRL + C to stop...")
+})
