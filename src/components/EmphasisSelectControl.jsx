63a8fddca9564e09a8bb8859d0d81cf4bf644fa9
DVo-MidwayUSA <dvo@midwayusa.com>
2019-10-17 14:53:12 -0500

    updated emphasis list

--- src/components/EmphasisSelectControl.jsx
+++ src/components/EmphasisSelectControl.jsx
@@ -9,19 +9,24 @@ const EmphasisSelectControl = props => {
         onChange={props.handleChange}
         required>
         <option value="">Select an area</option>
+        <option value="3rd Party API Integration">
+          3rd Party API Integration
+        </option>
+        <option value="A/B Testing">A/B Testing</option>
         <option value="Accessibility">Accessibility</option>
         <option value="Content Marketing">Content Marketing</option>
         <option value="Data Analysis">Data Analysis</option>
         <option value="Design Sprints">Design Sprints</option>
-        <option value="Security">Security</option>
-        <option value="SEO/SEM">SEO/SEM</option>
-        <option value="A/B Testing">A/B Testing</option>
+        <option value="DevOps">DevOps</option>
         <option value="Performance Optimization">
           Performance Optimization
         </option>
+        <option value="Quality Assurance">Quality Assurance</option>
         <option value="Research &amp; Development">
           Research &amp; Development
         </option>
+        <option value="Security">Security</option>
+        <option value="SEO/SEM">SEO/SEM</option>
       </select>
     </div>
   )
